72d8bd8e286022a5ea9d472ef52efc7cc90f5ca1
Eugene Rodriguez <er11@usf.edu>
2025-04-03 15:23:57 -0400

    Got bored so I made the message funny

--- src/components/Greeting.jsx
+++ src/components/Greeting.jsx
@@ -4,7 +4,9 @@ function Greeting() {
     return (
         <div>
             <h2>Hello from the Greeting Component!</h2>
-            <p>This is your first custom component in React.</p>
+            <p>This is your first custom component in React.
+                Now go outside feel the sun and maybe touch some grass.
+            </p>
         </div>
     );
 }
