09e48b66d826bc4bb33d338647c43bed6c1f1115
Eugene Rodriguez <er11@usf.edu>
2025-04-03 14:35:43 -0400

    changed greeting.jsx

--- src/components/Greeting.jsx
+++ src/components/Greeting.jsx
@@ -1,10 +1,12 @@
-const Greeting = () => {
+import Greeting from 'greeting';
+
+function appGreeting() {
     return (
         <div>
             <h2>Hello from the Greeting Component!</h2>
             <p>This is your first custom component in React.</p>
         </div>
     );
-};
+}
 
-export default Greeting;
+export default appGreeting;
